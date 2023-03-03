88764a90058a6a10a871262ef88922bacb25c8d3
Oleks Pickle <aleks.work2222+gh@gmail.com>
2023-03-03 20:45:58 +0100

    add some state handling

--- src/data.rs
+++ src/data.rs
@@ -1,6 +1,11 @@
 use anyhow::Result;
-use axum::{extract::State, http::StatusCode, response::Json};
+use axum::{
+    extract::{Extension, State},
+    http::StatusCode,
+    response::Json,
+};
 use lazy_static::lazy_static;
+use serde::Serialize;
 use serde_json::Value;
 use std::{
     collections::BTreeMap,
@@ -8,19 +13,24 @@ use std::{
     str::Bytes,
     sync::{Arc, Mutex},
 };
+use tinytemplate::TinyTemplate;
 use tokio::sync::mpsc::Receiver;
 use tokio::time::{sleep, Duration};
+
+/// Json file being persisted on disk
+const TMP_DIR: &str = "/tmp/updated-file.json";
+/// Template for visualisation
+const TEMPLATE: &'static str = include_str!("../assets/template.html");
+
+pub type SharedState = Arc<Mutex<AppState>>;
 
 lazy_static! {
     #[derive(Clone)]
     pub static ref STATE: SharedState = Arc::new(Mutex::new(AppState::default()));
 }
 
-const TMP_DIR: &str = "/tmp/updated-file.json";
-
-pub type SharedState = Arc<Mutex<AppState>>;
-
-pub async fn data_updater(tx: tokio::sync::mpsc::Sender<AppState>) -> Result<()> {
+/// Updates lazy shared state instance using persistent data or by querying some API
+pub async fn data_updater(tx: tokio::sync::mpsc::Sender<SharedState>) -> Result<()> {
     loop {
         if let Ok(mut state) = STATE.try_lock() {
             // TODO: some data requesting routine: persisted file reading, request update etc.
@@ -30,7 +40,8 @@ pub async fn data_updater(tx: tokio::sync::mpsc::Sender<AppState>) -> Result<()>
                 // are approaching more than 100 Kb, my dude...switch to some streaming or event based solution.
                 state.db.insert(k.to_string(), v.to_string());
             }
-            tx.send(state.clone()).await.unwrap();
+            drop(state);
+            tx.send(Arc::clone(&STATE)).await.unwrap();
         }
         sleep(Duration::from_secs(10)).await;
     }
@@ -47,23 +58,40 @@ impl AppState {
     }
 }
 
+#[derive(Serialize)]
+struct Context {
+    fields: Vec<(String, String)>,
+}
+
 pub mod handlers {
     use super::*;
 
     /// Main get state handler
-    pub async fn get_data(
-        State(state): State<SharedState>,
-        mut rx: Receiver<SharedState>,
-    ) -> Result<String, StatusCode> {
-        let map = &state
+    pub async fn get_data(mut rx: Receiver<SharedState>) -> Result<String, StatusCode> {
+        let map = &STATE
             .try_lock()
-            .map_err(|e| {
+            .or_else(|e| {
                 eprintln!("Error accessing state:{:?}", e);
-                Err(StatusCode::INTERNAL_SERVER_ERROR).into()
+                Err(StatusCode::INTERNAL_SERVER_ERROR)
             })?
             .db;
 
-        map.keys().map(|i| i.to_string()).collect()
+        if let Some(state) = rx.recv().await {
+            if &state.db != map {
+                let keys: Vec<String> = state.db.keys().map(ToString::to_string).collect();
+                let fields: Vec<(String, String)> = state.db.into_iter().collect();
+                let mut tt = TinyTemplate::new();
+                tt.add_template("main", TEMPLATE)
+                    .map_err(|e| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+                let rendered = tt
+                    .render("main", &Context { fields })
+                    .map_err(|e| StatusCode::INTERNAL_SERVER_ERROR)?;
+            }
+        }
+
+        let s = map.keys().map(|i| Ok(i.to_string())).collect();
+        s
     }
 
     pub async fn list_keys(State(state): State<SharedState>) -> Result<String> {
@@ -71,13 +99,14 @@ pub mod handlers {
             .try_lock()
             .map_err(|e| {
                 eprintln!("Error accessing state:{:?}", e);
-                Err(StatusCode::INTERNAL_SERVER_ERROR).into()
+                anyhow::format_err!("{}:{e}", StatusCode::INTERNAL_SERVER_ERROR)
             })?
             .db;
 
-        db.keys()
+        Ok(db
+            .keys()
             .map(|key| key.to_string())
             .collect::<Vec<String>>()
-            .join("\n")
+            .join("\n"))
     }
 }
